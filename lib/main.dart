d7242921d4a24fbae88288b402f63da93c0a62db
Alejandra GC <dev.ing.agc@outlook.com>
2024-09-01 22:30:25 -0600

    add: environment variable configuration

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@
 
 //Importaciones de flutter
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 //Importaciones de terceros
 
 
@@ -9,7 +10,9 @@ import 'package:flutter/material.dart';
 import 'package:cinemapedia/config/router/app_router.dart';
 import 'package:cinemapedia/config/theme/app_theme.dart';
 
-void main() {
+void main() async {
+  await dotenv.load(fileName: '.env');
+
   runApp(const MainApp());
 }
 
